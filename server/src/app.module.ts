4efac8eb26cb87259a6741cb067d4e7da4f041d4
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-08-18 09:29:06 +0300

    Setup static file serving by backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import config from "./config";
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: config.clientPath,
+      exclude: ["/api/(.*)"],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,8 +1,11 @@
+import { join } from "path";
+
 const { NODE_ENV, PORT } = process.env;
 
 const config = {
   inDev: NODE_ENV === "development",
   port: Number(PORT) || 3001,
+  clientPath: join(__dirname, "..", "public"),
 };
 
 export default config;
