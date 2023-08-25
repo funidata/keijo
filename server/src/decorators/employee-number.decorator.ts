45369121eecd370270055219a42bb3436e94d09d
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-08-25 10:28:01 +0300

    Add decorator to get employee number from headers

---
+++ server/src/decorators/employee-number.decorator.ts
@@ -0,0 +1,27 @@
+import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
+import { string } from "zod";
+
+const employeeNumberHeaderKey = "employeenumber";
+
+export const EmployeeNumber = createParamDecorator((_, context: ExecutionContext) => {
+  if (context.getType<GqlContextType>() !== "graphql") {
+    throw new BadRequestException("Server only accepts GraphQL requests.");
+  }
+
+  const request = GqlExecutionContext.create(context).getContext().req;
+
+  if (!Object.keys(request.headers).includes(employeeNumberHeaderKey)) {
+    throw new BadRequestException(`Header '${employeeNumberHeaderKey}' not found.`);
+  }
+
+  const employeeNumberHeaderValue = request.headers[employeeNumberHeaderKey];
+
+  try {
+    string().regex(/^\d+$/).transform(Number).parse(employeeNumberHeaderValue);
+  } catch {
+    throw new BadRequestException(`Header '${employeeNumberHeaderKey}' is not valid.`);
+  }
+
+  return parseInt(employeeNumberHeaderValue);
+});
