2529295112a822c3747b42b454bdc8020b1d9a5f
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-08-30 13:13:50 +0300

    Create mock configuration for unit tests

---
+++ server/test/utils/mock-config.service.ts
@@ -0,0 +1,31 @@
+import { Config } from "../../src/config/config.schema";
+import { ConfigService } from "../../src/config/config.service";
+
+const defaultMockConfig: Config = {
+  nodeEnv: "production",
+  inDev: false,
+  port: 3001,
+  employeeNumberHeaderKey: "test-shib-header",
+  netvisor: {
+    host: "https://mock-netvisor.url.com",
+    sender: "keijo",
+    partnerId: "test-partner-id",
+    lang: "fi",
+    customerId: "test-customer-id",
+    organizationId: "test-org-id",
+    customerKey: "test-customer-key",
+    organizationKey: "test-org-key",
+  },
+};
+
+const mockConfigProvider = (override: Partial<Config> = {}) => ({
+  provide: ConfigService,
+  useFactory: () => ({
+    config: {
+      ...defaultMockConfig,
+      ...override,
+    },
+  }),
+});
+
+export default mockConfigProvider;
