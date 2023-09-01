6afe1c4a51b6e391b10719445d488de84bf4bb29
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-01 13:22:46 +0300

    Install browser language detector

--- web/src/common/i18n.ts
+++ web/src/common/i18n.ts
@@ -1,19 +1,23 @@
 "use client";
 import i18next from "i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-i18next.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        asd: "Hello, world!",
+i18next
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          asd: "Hello, world!",
+        },
+      },
+      fi: {
+        translation: {
+          asd: "Moikka, maailma!",
+        },
       },
     },
-    fi: {
-      translation: {
-        asd: "Moikka, maailma!",
-      },
-    },
-  },
-  lng: "en",
-});
+    fallbackLng: "en",
+  });
