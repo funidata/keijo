b099baadb0ce9bd7dc730ef68643f907ef084c59
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-05 12:58:02 +0300

    Add dimension resolver

--- server/src/netvisor/dimension/dimension.resolver.ts
+++ server/src/netvisor/dimension/dimension.resolver.ts
@@ -1,9 +1,15 @@
 import { Query, Resolver } from "@nestjs/graphql";
 import { DimensionService } from "./dimension.service";
+import { Dimension } from "./dto/dimension.dto";
 
 @Resolver()
 export class DimensionResolver {
   constructor(private dimensionService: DimensionService) {}
+
+  @Query(() => [Dimension])
+  async findDimensions(): Promise<Dimension> {
+    return this.dimensionService.findAllDimensions();
+  }
 
   @Query(() => [String])
   async findDimensionNames(): Promise<string[]> {
