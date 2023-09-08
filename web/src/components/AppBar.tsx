fee8cbf97d86cfcffc8d6bee617e0fedcf864ed9
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-08 13:09:59 +0300

    Dialog and button boilerplate for adding entries

--- web/src/components/AppBar.tsx
+++ web/src/components/AppBar.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto";
 import { Box, Container, AppBar as MuiAppBar, Toolbar, Typography } from "@mui/material";
 import { ChildrenProps } from "../common/types";
 import "../i18n";
+import EntryDialogButton from "./EntryDialog/EntryDialogButton";
 import LanguageSelect from "./LanguageSelect";
 
 const AppBar = ({ children }: ChildrenProps) => (
@@ -12,6 +13,7 @@ const AppBar = ({ children }: ChildrenProps) => (
         <Typography variant="h6">Keijo</Typography>
         <Box sx={{ flexGrow: 1 }} />
         <Box>
+          <EntryDialogButton />
           <LanguageSelect />
         </Box>
       </Toolbar>
---
+++ web/src/components/EntryDialog/index.tsx
@@ -0,0 +1,9 @@
+import { Dialog, DialogProps, DialogTitle } from "@mui/material";
+
+const EntryDialog = (props: DialogProps) => (
+  <Dialog {...props}>
+    <DialogTitle>Entry</DialogTitle>
+  </Dialog>
+);
+
+export default EntryDialog;
