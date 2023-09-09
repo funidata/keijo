7102a4ef0a12b1374b26b4614a7daa8521653b04
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-09 22:02:52 +0300

    Add graphql query for record types

--- server/src/netvisor/record-type/record-type.resolver.ts
+++ server/src/netvisor/record-type/record-type.resolver.ts
@@ -1,7 +1,13 @@
-import { Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from "@nestjs/graphql";
+import { RecordType } from "./dto/record-type.dto";
 import { RecordTypeService } from "./record-type.service";
 
 @Resolver()
 export class RecordTypeResolver {
   constructor(private recordTypeService: RecordTypeService) {}
+
+  @Query(() => [RecordType])
+  async findRecordTypes() {
+    return this.recordTypeService.findAllRecordTypes();
+  }
 }
