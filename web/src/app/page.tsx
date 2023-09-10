641f2aa4e86cec0eb90a8c186a87c4d99ea113a3
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-10 20:32:14 +0300

    Fix entry list default date on sundays

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,13 +3,17 @@ import { useLazyQuery } from "@apollo/client";
 import { Paper } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
+import "dayjs/locale/fi";
+import weekday from "dayjs/plugin/weekday";
 import { useEffect, useState } from "react";
 import WorkdayAccordion from "../components/WorkdayAccordion";
 import { FindWorkdaysDocument } from "../graphql/generated/graphql";
 
+dayjs.extend(weekday);
+
 const Home = () => {
   const [findWorkdays, { data }] = useLazyQuery(FindWorkdaysDocument);
-  const [start, setStart] = useState<dayjs.Dayjs>(dayjs().day(1));
+  const [start, setStart] = useState<dayjs.Dayjs>(dayjs().locale("fi").weekday(0));
 
   useEffect(() => {
     const end = start.add(7, "day");
