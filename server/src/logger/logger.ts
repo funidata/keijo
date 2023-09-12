eee986b5102ac877848c0a8fe850e8dc5c833dcd
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-12 14:39:44 +0300

    Implement console logging for dev env

--- server/src/logger/logger.ts
+++ server/src/logger/logger.ts
@@ -1,9 +1,33 @@
-import { ConsoleLogger, Injectable, Scope } from "@nestjs/common";
+import { ConsoleLogger, Injectable, LogLevel, Scope } from "@nestjs/common";
 import { ConfigService } from "../config/config.service";
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class Logger extends ConsoleLogger {
   constructor(private configService: ConfigService) {
-    super();
+    super("", { logLevels: ["debug"] });
+  }
+
+  log(message: unknown, context?: string): void {
+    this.useConsoleLogger("log", message, context);
+  }
+
+  warn(message: unknown, context?: string): void {
+    this.useConsoleLogger("warn", message, context);
+  }
+
+  error(message: unknown, context?: string): void {
+    this.useConsoleLogger("error", message, context);
+  }
+
+  debug(message: unknown, context?: string): void {
+    this.useConsoleLogger("debug", message, context);
+  }
+
+  private useConsoleLogger(loggerFn: LogLevel, message: unknown, context?: string): void {
+    if (context === undefined) {
+      super[loggerFn](message);
+    } else {
+      super[loggerFn](message, context);
+    }
   }
 }
