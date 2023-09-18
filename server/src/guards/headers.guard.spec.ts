824b6b4250bbd50a7965673d0143579e2953a1a4
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-18 20:34:07 +0300

    Fix headers guard unit tests

--- server/src/guards/headers.guard.spec.ts
+++ server/src/guards/headers.guard.spec.ts
@@ -1,5 +1,6 @@
 import { createMock } from "@golevelup/ts-jest";
 import { AuthenticationError } from "@nestjs/apollo";
+import { Reflector } from "@nestjs/core";
 import { Test } from "@nestjs/testing";
 import mockConfigProvider, { defaultMockConfig } from "../../test/utils/mock-config.service";
 import { mockContextWithHeaders } from "../../test/utils/mock-context";
@@ -16,7 +17,9 @@ describe("HeadersGuard", () => {
     }).compile();
     const logger = createMock<Logger>();
     const configService = module.get(ConfigService);
-    guard = new HeadersGuard(logger, configService);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const reflector = createMock<Reflector>({ get: (): any => undefined });
+    guard = new HeadersGuard(logger, configService, reflector);
   });
 
   it("Allows activation with valid headers", () => {
@@ -42,5 +45,34 @@ describe("HeadersGuard", () => {
   it("Denies activation with an invalid employee number header", () => {
     const ctx = mockContextWithHeaders({ [defaultMockConfig.employeeNumberHeaderKey]: "123a" });
     expect(() => guard.canActivate(ctx)).toThrowError(AuthenticationError);
+  });
+
+  describe("With bypass on", () => {
+    beforeEach(async () => {
+      const module = await Test.createTestingModule({
+        providers: [mockConfigProvider()],
+      }).compile();
+      const logger = createMock<Logger>();
+      const configService = module.get(ConfigService);
+      const reflector = createMock<Reflector>({ get: () => true });
+      guard = new HeadersGuard(logger, configService, reflector);
+    });
+
+    it("Allows activation with valid headers", () => {
+      const ctx = mockContextWithHeaders({ [defaultMockConfig.employeeNumberHeaderKey]: "123" });
+      expect(guard.canActivate(ctx)).toBeTruthy();
+    });
+
+    it("Allows activation with undefined employee number header", () => {
+      const ctx = mockContextWithHeaders({
+        [defaultMockConfig.employeeNumberHeaderKey]: undefined,
+      });
+      expect(guard.canActivate(ctx)).toBeTruthy();
+    });
+
+    it("Allows activation with an invalid employee number header", () => {
+      const ctx = mockContextWithHeaders({ [defaultMockConfig.employeeNumberHeaderKey]: "123a" });
+      expect(guard.canActivate(ctx)).toBeTruthy();
+    });
   });
 });
