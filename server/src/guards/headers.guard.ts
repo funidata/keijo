ffb13217be6df00460dadba2a4a52ef99989148e
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-18 20:19:31 +0300

    Fix missing employee number when bypassing guard

--- server/src/guards/headers.guard.ts
+++ server/src/guards/headers.guard.ts
@@ -9,41 +9,51 @@ import { Logger } from "../logger/logger";
 
 @Injectable()
 export class HeadersGuard implements CanActivate {
+  private employeeNumberHeaderKey: string;
+
   constructor(
     private logger: Logger,
-    private configService: ConfigService,
+    configService: ConfigService,
     private readonly reflector: Reflector,
   ) {
     logger.setContext(HeadersGuard.name);
+    this.employeeNumberHeaderKey = configService.config.employeeNumberHeaderKey.toLowerCase();
   }
 
   canActivate(context: ExecutionContext): boolean {
+    // Guard can be bypassed using @BypassHeadersGuard decorator.
     const bypass = getBypassHeadersGuardMetadataValue(this.reflector, context);
     if (bypass) {
+      this.appendEmployeeNumberToRequest(context);
       return true;
     }
 
-    const employeeNumberHeaderKey = this.configService.config.employeeNumberHeaderKey.toLowerCase();
     const request = GqlExecutionContext.create(context).getContext().req;
 
-    if (!Object.keys(request.headers).includes(employeeNumberHeaderKey)) {
-      const message = `Header '${employeeNumberHeaderKey}' not found.`;
+    if (!Object.keys(request.headers).includes(this.employeeNumberHeaderKey)) {
+      const message = `Header '${this.employeeNumberHeaderKey}' not found.`;
       this.logger.error(message);
       throw new AuthenticationError(message);
     }
 
-    const employeeNumberHeaderValue = request.headers[employeeNumberHeaderKey];
+    const employeeNumberHeaderValue = request.headers[this.employeeNumberHeaderKey];
 
     try {
       string().regex(/^\d+$/).transform(Number).parse(employeeNumberHeaderValue);
     } catch {
-      const message = `Header '${employeeNumberHeaderKey}' is not valid.`;
+      const message = `Header '${this.employeeNumberHeaderKey}' is not valid.`;
       this.logger.error(message);
       throw new AuthenticationError(message);
     }
 
-    request.employeeNumber = parseInt(employeeNumberHeaderValue);
+    this.appendEmployeeNumberToRequest(context);
 
     return true;
+  }
+
+  private appendEmployeeNumberToRequest(context: ExecutionContext): void {
+    const request = GqlExecutionContext.create(context).getContext().req;
+    const employeeNumberHeaderValue = request.headers[this.employeeNumberHeaderKey];
+    request.employeeNumber = parseInt(employeeNumberHeaderValue) || undefined;
   }
 }
