e42a506b5213ef4f657e3bc89222129a508d71f9
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-18 13:40:49 +0300

    Remove duplicate code

--- server/src/decorators/employee-number.decorator.ts
+++ server/src/decorators/employee-number.decorator.ts
@@ -1,25 +1,10 @@
-import {
-  BadRequestException,
-  createParamDecorator,
-  ExecutionContext,
-  Logger,
-} from "@nestjs/common";
-import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
 import config from "../config/config";
 
-// TODO: Move everything else to middleware (or validation pipes?) and leave only employee number fetching here.
-// This is probably not the place to inject services etc.
-// This should return undefined, if the header was not found(?).
+// FIXME: This should return undefined, if the header was not found(?).
 
 export const EmployeeNumber = createParamDecorator((_, context: ExecutionContext) => {
-  const logger = new Logger("EmployeeNumber");
-
-  if (context.getType<GqlContextType>() !== "graphql") {
-    const message = "Server only accepts GraphQL requests.";
-    logger.error(message);
-    throw new BadRequestException(message);
-  }
-
   const employeeNumberHeaderKey = config.employeeNumberHeaderKey.toLowerCase();
   const request = GqlExecutionContext.create(context).getContext().req;
   const employeeNumberHeaderValue = request.headers[employeeNumberHeaderKey];
