3ff1746a230d513c8ed3bb26111f0b54dfa711fb
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-19 17:19:19 +0300

    Refactor

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,43 +1,8 @@
 "use client";
-import { useLazyQuery } from "@apollo/client";
-import { Paper } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers";
-import { Dayjs } from "dayjs";
-import { useEffect, useState } from "react";
-import dayjs from "../common/dayjs";
-import WorkdayAccordion from "../components/WorkdayAccordion";
-import { FindWorkdaysDocument } from "../graphql/generated/graphql";
+import WorkdayBrowser from "../components/WorkdayBrowser";
 
 const Home = () => {
-  const [findWorkdays, { data }] = useLazyQuery(FindWorkdaysDocument);
-  const [start, setStart] = useState<Dayjs>(dayjs().locale("fi").weekday(0));
-
-  useEffect(() => {
-    const end = start.add(7, "day");
-    findWorkdays({ variables: { start, end } });
-  }, [start, findWorkdays]);
-
-  const handleStartChange = (val: any) => {
-    const date = dayjs(val);
-    if (date.isValid()) {
-      setStart(date);
-    }
-  };
-
-  if (!data) {
-    return null;
-  }
-
-  return (
-    <>
-      <DatePicker value={start} onChange={handleStartChange} />
-      <Paper>
-        {data.findWorkdays.map((wd) => (
-          <WorkdayAccordion workday={wd} key={wd.date} />
-        ))}
-      </Paper>
-    </>
-  );
+  return <WorkdayBrowser />;
 };
 
 export default Home;
---
+++ web/src/components/WorkdayBrowser/index.tsx
@@ -0,0 +1,42 @@
+import { useLazyQuery } from "@apollo/client";
+import { Paper } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import { Dayjs } from "dayjs";
+import { useEffect, useState } from "react";
+import dayjs from "../../common/dayjs";
+import { FindWorkdaysDocument } from "../../graphql/generated/graphql";
+import WorkdayAccordion from "../WorkdayAccordion";
+
+const WorkdayBrowser = () => {
+  const [findWorkdays, { data }] = useLazyQuery(FindWorkdaysDocument);
+  const [start, setStart] = useState<Dayjs>(dayjs().locale("fi").weekday(0));
+
+  useEffect(() => {
+    const end = start.add(7, "day");
+    findWorkdays({ variables: { start, end } });
+  }, [start, findWorkdays]);
+
+  const handleStartChange = (val: any) => {
+    const date = dayjs(val);
+    if (date.isValid()) {
+      setStart(date);
+    }
+  };
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      <DatePicker value={start} onChange={handleStartChange} />
+      <Paper>
+        {data.findWorkdays.map((wd) => (
+          <WorkdayAccordion workday={wd} key={wd.date} />
+        ))}
+      </Paper>
+    </>
+  );
+};
+
+export default WorkdayBrowser;
