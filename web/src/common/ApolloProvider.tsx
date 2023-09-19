94236dbdd735511c5f1b3bf3060327178538e6ab
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-19 14:43:01 +0300

    Raise global notifications on all GraphQL errors

--- web/src/common/ApolloProvider.tsx
+++ web/src/common/ApolloProvider.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { HttpLink } from "@apollo/client";
+import { HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import {
   ApolloNextAppProvider,
   NextSSRApolloClient,
   NextSSRInMemoryCache,
 } from "@apollo/experimental-nextjs-app-support/ssr";
+import { useNotificationState } from "../components/GlobalNotification/useNotification";
 import { ChildrenProps } from "./types";
 
 const makeClient = () => {
@@ -16,9 +18,28 @@ const makeClient = () => {
         : { "x-shib-employeeid": process.env.NEXT_PUBLIC_MOCK_EMPLOYEE_NUMBER || "" },
   });
 
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, path }) =>
+        useNotificationState.getState().setNotification({
+          message: `Error in GraphQL request "${path}". Message: ${message}`,
+          type: "error",
+          autoHide: false,
+        }),
+      );
+
+    if (networkError) {
+      useNotificationState.getState().setNotification({
+        message: `GraphQL network error: ${networkError}`,
+        type: "error",
+        autoHide: false,
+      });
+    }
+  });
+
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
-    link: httpLink,
+    link: from([errorLink, httpLink]),
   });
 };
 
