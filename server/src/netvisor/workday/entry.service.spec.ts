d58f7721f133bf216c4eed13e86cea2c17b4cc71
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-11 14:11:01 +0200

    Fix entry service tests

--- server/src/netvisor/workday/entry.service.spec.ts
+++ server/src/netvisor/workday/entry.service.spec.ts
@@ -18,7 +18,7 @@ describe("EntryService", () => {
   const date = dayjs();
   const employeeNumber = 123;
 
-  let service: EntryService;
+  let entryService: EntryService;
   let workdayService: WorkdayService;
   let netvisorApiService: NetvisorApiService;
   let logger: Logger;
@@ -29,7 +29,7 @@ describe("EntryService", () => {
       providers: [EntryService, MockNetvisorApiService, MockWorkdayService, MockLogger],
     }).compile();
 
-    service = module.get(EntryService);
+    entryService = module.get(EntryService);
     workdayService = module.get(WorkdayService);
     netvisorApiService = module.get(NetvisorApiService);
     logger = module.get(Logger);
@@ -38,46 +38,52 @@ describe("EntryService", () => {
     };
   });
 
+  const entry = {
+    key,
+    duration: 1,
+    entryType: "",
+    product: "",
+    activity: "",
+    issue: "",
+    client: "",
+  };
+
   describe("findOne", () => {
     it("Returns the correct entry", () => {
-      const entry = { key: "100", duration: 1, entryType: "", dimensions: [] };
       mockResolvedWorkday([
         {
           date: new Date(),
-          entries: [{ key: "101", duration: 1, entryType: "", dimensions: [] }, entry],
+          entries: [{ ...entry, key: "101" }, entry],
         },
       ]);
-      expect(service.findOne(key, date, employeeNumber)).resolves.toMatchObject(entry);
+      expect(entryService.findOne(key, date, employeeNumber)).resolves.toMatchObject(entry);
     });
 
     it("Returns undefined when workday does not exist", () => {
       mockResolvedWorkday([]);
-      expect(service.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
+      expect(entryService.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
     });
 
     it("Returns undefined when entries do not exist", () => {
       mockResolvedWorkday([{ date: new Date(), entries: [] }]);
-      expect(service.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
+      expect(entryService.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
     });
 
     it("Returns undefined when entry is not found", () => {
-      mockResolvedWorkday([
-        { date: new Date(), entries: [{ key: "101", duration: 1, entryType: "", dimensions: [] }] },
-      ]);
-      expect(service.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
+      mockResolvedWorkday([{ date: new Date(), entries: [{ ...entry, key: "101" }] }]);
+      expect(entryService.findOne(key, date, employeeNumber)).resolves.toBeUndefined();
     });
   });
 
   describe("remove", () => {
     it("Removes the requested entry", async () => {
-      const entry = { key, duration: 1, entryType: "", dimensions: [] };
       mockResolvedWorkday([
         {
           date: new Date(),
           entries: [entry],
         },
       ]);
-      await service.remove(employeeNumber, "", key, date);
+      await entryService.remove(employeeNumber, "", key, date);
 
       expect(netvisorApiService.get).toBeCalledTimes(1);
       expect(netvisorApiService.get).toBeCalledWith(NetvisorEndpoints.DELETE_WORKDAYHOUR, [], {
@@ -88,12 +94,11 @@ describe("EntryService", () => {
 
     it("Throws on missing entry", async () => {
       mockResolvedWorkday([]);
-      const res = service.remove(employeeNumber, "", key, date);
+      const res = entryService.remove(employeeNumber, "", key, date);
       await expect(res).rejects.toThrow(NotFoundException);
     });
 
     it("Throws on failed API operation", async () => {
-      const entry = { key, duration: 1, entryType: "", dimensions: [] };
       mockResolvedWorkday([
         {
           date: new Date(),
@@ -103,7 +108,7 @@ describe("EntryService", () => {
       jest
         .spyOn(netvisorApiService, "get")
         .mockResolvedValueOnce({ Root: { ResponseStatus: { Status: "FAILED" } } });
-      const res = service.remove(employeeNumber, "", key, date);
+      const res = entryService.remove(employeeNumber, "", key, date);
       await expect(res).rejects.toThrow(BadRequestException);
     });
   });
