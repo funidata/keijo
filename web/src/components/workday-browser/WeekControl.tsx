12110cbe96ee8442c23ddcde466f813c4a93bc75
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-18 20:13:37 +0200

    Implement changing weeks with dropdown and arrows

--- web/src/components/workday-browser/WeekControl.tsx
+++ web/src/components/workday-browser/WeekControl.tsx
@@ -1,36 +1,89 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ErrorIcon from "@mui/icons-material/Error";
-import { Box, Chip, FormControl, IconButton, InputLabel, MenuItem, Select } from "@mui/material";
-import dayjs from "dayjs";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  SxProps,
+  Typography,
+} from "@mui/material";
+import { Dayjs } from "dayjs";
+import { range } from "lodash";
+import dayjs from "../../common/dayjs";
 import useWorkdayBrowser from "./useWorkdayBrowser";
 
-// TODO: Acually implement this.
-
 const WeekControl = () => {
-  const { start } = useWorkdayBrowser();
-  const currentWeek = start.week();
-  console.log(currentWeek);
+  const { start, setStart, setEnd } = useWorkdayBrowser();
+  const selectedValue = start.week().toString();
 
-  console.log(dayjs().week(0).week());
+  // Current (NOT selected!) week serves as middle point for dropdown options.
+  const currentWeek = dayjs().week();
+  const weeks = range(currentWeek - 5, currentWeek + 5).map((weekIndex) => ({
+    no: dayjs().week(weekIndex).week(),
+    // Note: Calculating start date from the index guarantees correct dates even when the year changes.
+    start: dayjs().week(weekIndex).weekday(0),
+  }));
+
+  const selectWeek = (start: Dayjs) => {
+    setStart(start);
+    setEnd(start.add(6, "day"));
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const value = Number(event.target.value);
+    const start = weeks.find((week) => week.no === value)?.start || dayjs();
+    selectWeek(start);
+  };
+
+  const goToPreviousWeek = () => selectWeek(start.subtract(7, "day"));
+  const goToNextWeek = () => selectWeek(start.add(7, "day"));
+
+  const iconButtonSx: SxProps = { borderRadius: 1, pl: 2, pr: 2 };
 
   return (
-    <Box>
-      <Box>
-        <Chip label="Not implemented, use Browse by Date." color="error" icon={<ErrorIcon />} />
-      </Box>
-      <IconButton disabled>
+    <Box sx={{ display: "flex", justifyContent: "center", gap: 1 }}>
+      <IconButton sx={iconButtonSx} onClick={goToPreviousWeek}>
         <ArrowBackIcon />
       </IconButton>
-      <FormControl sx={{ minWidth: 200 }}>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Age" disabled>
-          <MenuItem value={10}>52</MenuItem>
-          <MenuItem value={20}>1</MenuItem>
-          <MenuItem value={30}>2</MenuItem>
+      <FormControl sx={{ minWidth: 100 }}>
+        <InputLabel id="demo-simple-select-label">Week</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Week"
+          value={selectedValue}
+          onChange={handleChange}
+          sx={{
+            [".MuiBox-root"]: {
+              border: 0,
+            },
+          }}
+        >
+          {weeks.map((week) => (
+            <MenuItem value={week.no} key={`week-control-menu-item-${week.no}`}>
+              <Box
+                sx={{
+                  display: "flex",
+                  borderBottom: week.no === currentWeek ? "1px solid grey" : null,
+                }}
+              >
+                <Typography variant="h5">{week.no}</Typography>
+                <Typography
+                  variant="subtitle2"
+                  sx={{ alignSelf: "end", mb: "3px", ml: 1, fontStyle: "italic" }}
+                >
+                  ({week.start.format("l")})
+                </Typography>
+              </Box>
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
-      <IconButton disabled>
+      <IconButton sx={iconButtonSx} onClick={goToNextWeek}>
         <ArrowForwardIcon />
       </IconButton>
     </Box>
