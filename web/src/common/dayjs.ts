f265e76c63c6090d6bbe779422ae2d4b6a159dc2
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-19 21:06:39 +0200

    Include end date in results

--- web/src/common/dayjs.ts
+++ web/src/common/dayjs.ts
@@ -1,13 +1,16 @@
 import _dayjs from "dayjs";
 import "dayjs/locale/en-gb";
 import "dayjs/locale/fi";
-import weekday from "dayjs/plugin/weekday";
+import utc from "dayjs/plugin/utc";
 import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekday from "dayjs/plugin/weekday";
 
 const dayjs = _dayjs;
 
 dayjs.extend(weekday);
 dayjs.extend(weekOfYear);
+dayjs.extend(utc);
+
 dayjs.locale("fi");
 
 export default dayjs;
--- web/src/components/workday-browser/WorkdayList.tsx
+++ web/src/components/workday-browser/WorkdayList.tsx
@@ -8,7 +8,10 @@ import useWorkdayBrowser from "./useWorkdayBrowser";
 const WorkdayList = () => {
   const { t } = useTranslation();
   const { start, end } = useWorkdayBrowser();
-  const { data } = useQuery(FindWorkdaysDocument, { variables: { start, end } });
+
+  const { data } = useQuery(FindWorkdaysDocument, {
+    variables: { start: start.format("YYYY-MM-DD"), end: end.format("YYYY-MM-DD") },
+  });
 
   if (!data) {
     return (
