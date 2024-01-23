40137119aa01180e1b3cf2611f082814efcb0170
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-23 10:08:31 +0200

    Include days without entries in workday list

--- web/src/components/workday-browser/WorkdayList.tsx
+++ web/src/components/workday-browser/WorkdayList.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from "@apollo/client";
 import { Paper } from "@mui/material";
+import { range } from "lodash";
+import useDayjs from "../../common/useDayjs";
 import { FindWorkdaysDocument } from "../../graphql/generated/graphql";
 import WorkdayAccordion from "../workday-accordion/WorkdayAccordion";
 import LoadingIndicator from "./LoadingIndicator";
 import useWorkdayBrowser from "./useWorkdayBrowser";
 
 const WorkdayList = () => {
+  const dayjs = useDayjs();
   const { start, end } = useWorkdayBrowser();
 
   const { data } = useQuery(FindWorkdaysDocument, {
@@ -16,10 +19,22 @@ const WorkdayList = () => {
     return <LoadingIndicator />;
   }
 
+  const normalizedStart = start.hour(0).minute(0).second(0).millisecond(0);
+  const normalizedEnd = end.hour(0).minute(0).second(0).millisecond(0);
+
+  // Construct requested date range to include days without entries.
+  const workdays = range(normalizedEnd.diff(normalizedStart, "day") + 1).map((i) => {
+    const date = normalizedStart.add(i, "day");
+    return {
+      date,
+      entries: data.findWorkdays.find((wd) => date.isSame(dayjs(wd.date), "day"))?.entries || [],
+    };
+  });
+
   return (
     <Paper>
-      {data.findWorkdays.map((wd) => (
-        <WorkdayAccordion workday={wd} key={wd.date} />
+      {workdays.map((wd) => (
+        <WorkdayAccordion workday={wd} key={wd.date.toString()} />
       ))}
     </Paper>
   );
