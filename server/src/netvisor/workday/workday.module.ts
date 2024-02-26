1b9e3fbcb6e56c6c93f9d327167c8d80f53e5abf
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-02-26 10:17:47 +0200

    Use axios through module rather than directly

--- server/src/netvisor/workday/workday.module.ts
+++ server/src/netvisor/workday/workday.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { NetvisorApiModule } from "../netvisor-api/netvisor-api.module";
-import { NetvisorApiService } from "../netvisor-api/netvisor-api.service";
 import { EntryService } from "./entry.service";
 import { WorkdayResolver } from "./workday.resolver";
 import { WorkdayService } from "./workday.service";
 
 @Module({
   imports: [NetvisorApiModule],
-  providers: [WorkdayResolver, WorkdayService, EntryService, NetvisorApiService],
+  providers: [WorkdayResolver, WorkdayService, EntryService],
 })
 export class WorkdayModule {}
