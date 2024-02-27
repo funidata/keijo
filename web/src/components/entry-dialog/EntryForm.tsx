8dbcb758423c6ddd635b5d115c048f78fa7331ff
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-02-27 09:58:38 +0200

    Separate entry dialog button layout between mobile and desktop viewports

--- web/src/components/entry-dialog/EntryForm.tsx
+++ web/src/components/entry-dialog/EntryForm.tsx
@@ -39,16 +39,31 @@ const EntryForm = ({ control, reset, onSubmit, editMode }: EntryFormProps) => {
         <DimensionSelect control={control} name="activity" title={t("entryDialog.activity")} />
         <DimensionSelect control={control} name="issue" title={t("entryDialog.issue")} />
         <DimensionSelect control={control} name="client" title={t("entryDialog.client")} />
-        <Grid item xs={12}>
-          <Box sx={{ display: "flex", justifyContent: "end", gap: 2 }}>
-            <Button type="reset" variant="outlined" size="large" onClick={reset}>
-              {editMode ? t("entryDialog.reset") : t("entryDialog.clear")}
-            </Button>
-            <Button type="submit" variant="contained" size="large">
-              {t("entryDialog.submit")}
-            </Button>
-          </Box>
-        </Grid>
+        {mobile ? (
+          <>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" size="large" fullWidth>
+                {t("entryDialog.submit")}
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="reset" variant="outlined" size="large" onClick={reset} fullWidth>
+                {editMode ? t("entryDialog.reset") : t("entryDialog.clear")}
+              </Button>
+            </Grid>
+          </>
+        ) : (
+          <Grid item xs={12}>
+            <Box sx={{ display: "flex", justifyContent: "end", gap: 2 }}>
+              <Button type="reset" variant="outlined" size="large" onClick={reset}>
+                {editMode ? t("entryDialog.reset") : t("entryDialog.clear")}
+              </Button>
+              <Button type="submit" variant="contained" size="large">
+                {t("entryDialog.submit")}
+              </Button>
+            </Box>
+          </Grid>
+        )}
       </Grid>
     </form>
   );
