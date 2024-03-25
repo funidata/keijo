52d7da27e03e02cf2e3bf94b1926830f5f60e9a6
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-03-25 16:14:51 +0200

    Refactor dimension chip to include dimension-specific styling

--- web/src/components/workday-accordion/DimensionChip.tsx
+++ web/src/components/workday-accordion/DimensionChip.tsx
@@ -1,7 +1,32 @@
-import { Chip, ChipProps } from "@mui/material";
+import BuildIcon from "@mui/icons-material/Build";
+import LayersIcon from "@mui/icons-material/Layers";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import PersonIcon from "@mui/icons-material/Person";
+import { Chip, ChipProps, SvgIconProps } from "@mui/material";
 
-const DimensionChip = (props: ChipProps) => (
-  <Chip variant="outlined" sx={{ borderRadius: 1 }} {...props} />
-);
+const icons = {
+  product: LayersIcon,
+  activity: BuildIcon,
+  issue: LocalOfferIcon,
+  client: PersonIcon,
+};
+
+type DimensionChipProps = ChipProps & {
+  dimension: "activity" | "issue" | "client" | "product";
+};
+
+const DimensionChip = ({ dimension, sx, ...props }: DimensionChipProps) => {
+  const Icon = icons[dimension];
+  const iconCommonProps: SvgIconProps = { fontSize: "small" };
+
+  return (
+    <Chip
+      icon={<Icon {...iconCommonProps} />}
+      variant="outlined"
+      sx={{ borderRadius: 1, ...sx }}
+      {...props}
+    />
+  );
+};
 
 export default DimensionChip;
