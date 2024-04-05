b749a0b8af00f31514723ae5baf568fa2790c80e
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-04-05 14:58:02 +0300

    Refactor chips

--- web/src/components/workday-accordion/WorkdaySummary.tsx
+++ web/src/components/workday-accordion/WorkdaySummary.tsx
@@ -1,19 +1,19 @@
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { AccordionSummary, Box, Chip, Typography } from "@mui/material";
 import { sum } from "lodash";
-import { useTranslation } from "react-i18next";
 import { roundToFullMinutes } from "../../common/duration";
 import { isHoliday } from "../../common/isHoliday";
 import useDayjs from "../../common/useDayjs";
 import { Workday } from "../../graphql/generated/graphql";
 import EntryDialogButton from "../entry-dialog/EntryDialogButton";
+import HolidayChip from "./info-chips/HolidayChip";
+import NoEntriesChip from "./info-chips/NoEntriesChip";
 
 type WorkdayAccordionProps = {
   workday: Workday;
 };
 
 const WorkdaySummary = ({ workday }: WorkdayAccordionProps) => {
-  const { t } = useTranslation();
   const dayjs = useDayjs();
   const date = dayjs(workday.date).locale(dayjs.locale());
   const holiday = isHoliday(date);
@@ -35,27 +35,8 @@ const WorkdaySummary = ({ workday }: WorkdayAccordionProps) => {
         }}
       >
         <Typography sx={{ textTransform: "capitalize" }}>{date.format("dd l")}</Typography>
-        {holiday && (
-          <Chip
-            label={t("entryTable.holiday")}
-            variant="filled"
-            sx={{
-              color: "inherit",
-              fontWeight: 500,
-              bgcolor: (theme) =>
-                theme.palette.mode === "dark"
-                  ? theme.palette.secondary.dark
-                  : theme.palette.primary.light,
-            }}
-          />
-        )}
-        {!holiday && empty && (
-          <Chip
-            label={t("entryTable.noEntries")}
-            variant="outlined"
-            style={{ color: "inherit", fontStyle: "italic" }}
-          />
-        )}
+        {holiday && <HolidayChip />}
+        {!holiday && empty && <NoEntriesChip />}
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <EntryDialogButton date={date} size="medium" />
           <Chip label={`${totalHoursFormatted} h`} sx={{ mr: 2, color: "inherit" }} />
