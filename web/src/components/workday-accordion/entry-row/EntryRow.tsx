2c7f58524ac6692bfe63b38b70300cd7e8431c3d
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-04-08 12:37:56 +0300

    Wrap dimension chips and comment instead of hiding them behind horizontal scrolling

--- web/src/components/workday-accordion/entry-row/EntryRow.tsx
+++ web/src/components/workday-accordion/entry-row/EntryRow.tsx
@@ -1,108 +1,19 @@
-import { Box, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
-import dayjs, { Dayjs } from "dayjs";
-import { roundToFullMinutes } from "../../../common/duration";
-import { AcceptanceStatus, Entry } from "../../../graphql/generated/graphql";
-import useDarkMode from "../../../theme/useDarkMode";
-import DeleteEntryButton from "./DeleteEntryButton";
-import DimensionChip from "./DimensionChip";
-import EditEntryButton from "./EditEntryButton";
-import AcceptedChip from "./status-chips/AcceptedChip";
-import OpenChip from "./status-chips/OpenChip";
-import PaidChip from "./status-chips/PaidChip";
+import { useMediaQuery, useTheme } from "@mui/material";
+import { Dayjs } from "dayjs";
+import { Entry } from "../../../graphql/generated/graphql";
+import DesktopEntryRow from "./DesktopEntryRow";
+import MobileEntryRow from "./MobileEntryRow";
 
-type EntryFlexRowProps = {
+export type EntryRowProps = {
   entry: Entry;
   date: Dayjs;
 };
 
-const EntryRow = ({ entry, date }: EntryFlexRowProps) => {
-  const { darkMode } = useDarkMode();
-  const { product, activity, issue, client, description } = entry;
-  const accepted = entry.acceptanceStatus === AcceptanceStatus.Accepted;
-  const paid = entry.acceptanceStatus === AcceptanceStatus.Paid;
-  const open = entry.acceptanceStatus === AcceptanceStatus.Open;
-  const roundedDuration = roundToFullMinutes(dayjs.duration(entry.duration, "hour"));
+const EntryRow = (props: EntryRowProps) => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("md"));
 
-  return (
-    <Box
-      sx={{
-        bgcolor: darkMode ? grey[800] : "primary.light",
-        borderRadius: 4,
-        pl: 1,
-        pr: accepted || paid || open ? 0 : 1,
-        overflow: "hidden",
-        display: "flex",
-        alignItems: "stretch",
-        justifyContent: "space-between",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: 2,
-          overflowX: { xs: "auto", md: "hidden" },
-          overflowY: "hidden",
-          whiteSpace: "nowrap",
-          mr: 1,
-          pt: 1,
-          pb: 1,
-          minHeight: 48,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "end",
-            minWidth: 60,
-            textAlign: "right",
-            mr: 1,
-          }}
-        >
-          <Typography variant="h6">{roundedDuration.format("HH:mm")}</Typography>
-        </Box>
-        {product && <DimensionChip dimension="product" label={product} />}
-        {activity && <DimensionChip dimension="activity" label={activity} />}
-        {issue && <DimensionChip dimension="issue" label={issue} />}
-        {client && <DimensionChip dimension="client" label={client} />}
-        {description && (
-          <Typography
-            variant="subtitle2"
-            sx={{ overflow: { xs: "visible", md: "hidden" }, textOverflow: "ellipsis" }}
-          >
-            {description}
-          </Typography>
-        )}
-      </Box>
-      <Box sx={{ display: "flex" }}>
-        {accepted ? (
-          <Box>
-            <AcceptedChip />
-          </Box>
-        ) : paid ? (
-          <Box>
-            <PaidChip />
-          </Box>
-        ) : (
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Box>
-              <EditEntryButton date={date} entry={entry} />
-            </Box>
-            <Box sx={{ display: { xs: "none", md: "block" }, ml: -0.5 }}>
-              <DeleteEntryButton date={date} entryKey={entry.key} />
-            </Box>
-          </Box>
-        )}
-        {open && (
-          <Box>
-            <OpenChip />
-          </Box>
-        )}
-      </Box>
-    </Box>
-  );
+  return mobile ? <MobileEntryRow {...props} /> : <DesktopEntryRow {...props} />;
 };
 
 export default EntryRow;
---
+++ web/src/components/workday-accordion/entry-row/MobileEntryRow.tsx
@@ -0,0 +1,98 @@
+import { Box, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import dayjs from "dayjs";
+import { roundToFullMinutes } from "../../../common/duration";
+import { AcceptanceStatus } from "../../../graphql/generated/graphql";
+import useDarkMode from "../../../theme/useDarkMode";
+import DeleteEntryButton from "./DeleteEntryButton";
+import DimensionChip from "./DimensionChip";
+import EditEntryButton from "./EditEntryButton";
+import { EntryRowProps } from "./EntryRow";
+import AcceptedChip from "./status-chips/AcceptedChip";
+import OpenChip from "./status-chips/OpenChip";
+import PaidChip from "./status-chips/PaidChip";
+
+const MobileEntryRow = ({ entry, date }: EntryRowProps) => {
+  const { darkMode } = useDarkMode();
+  const { product, activity, issue, client, description } = entry;
+  const accepted = entry.acceptanceStatus === AcceptanceStatus.Accepted;
+  const paid = entry.acceptanceStatus === AcceptanceStatus.Paid;
+  const open = entry.acceptanceStatus === AcceptanceStatus.Open;
+  const roundedDuration = roundToFullMinutes(dayjs.duration(entry.duration, "hour"));
+
+  return (
+    <Box
+      sx={{
+        bgcolor: darkMode ? grey[800] : "primary.light",
+        borderRadius: 4,
+        pl: 1,
+        pr: accepted || paid || open ? 0 : 1,
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "end",
+            minWidth: 50,
+            mr: 1,
+          }}
+        >
+          <Typography variant="h6">{roundedDuration.format("H:mm")}</Typography>
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          {accepted ? (
+            <Box>
+              <AcceptedChip />
+            </Box>
+          ) : paid ? (
+            <Box>
+              <PaidChip />
+            </Box>
+          ) : (
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Box>
+                <EditEntryButton date={date} entry={entry} />
+              </Box>
+              <Box sx={{ display: "block", ml: -0.5 }}>
+                <DeleteEntryButton date={date} entryKey={entry.key} />
+              </Box>
+            </Box>
+          )}
+          {open && (
+            <Box>
+              <OpenChip />
+            </Box>
+          )}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          gap: 1,
+          mr: 1,
+          pb: 1,
+          minHeight: 48,
+        }}
+      >
+        {product && <DimensionChip dimension="product" label={product} />}
+        {activity && <DimensionChip dimension="activity" label={activity} />}
+        {issue && <DimensionChip dimension="issue" label={issue} />}
+        {client && <DimensionChip dimension="client" label={client} />}
+        {description && (
+          <Typography variant="subtitle2" sx={{ width: "100%", mt: 1, ml: 1 }}>
+            {description}
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default MobileEntryRow;
