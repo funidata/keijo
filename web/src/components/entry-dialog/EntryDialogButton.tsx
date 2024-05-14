ba8ab12a60cff4deb8dd4ad386b5ddad31555809
Kimi Kuru <kimi.kuru@funidata.fi>
2024-05-14 17:16:23 +0300

    use generatePath to strip possible doube slash

--- web/src/components/entry-dialog/EntryDialogButton.tsx
+++ web/src/components/entry-dialog/EntryDialogButton.tsx
@@ -2,7 +2,7 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { Box, IconButton, IconButtonProps, Tooltip } from "@mui/material";
 import { Dayjs } from "dayjs";
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate } from "react-router-dom";
+import { generatePath, useLocation, useNavigate } from "react-router-dom";
 
 type EntryDialogButtonProps = IconButtonProps & {
   date?: Dayjs;
@@ -18,7 +18,7 @@ const EntryDialogButton = ({ date, ...props }: EntryDialogButtonProps) => {
       <Tooltip title={t("entryDialog.title")}>
         <IconButton
           aria-label={t("entryDialog.title")}
-          onClick={() => navigate(`${location.pathname}/create`, { state: { date } })}
+          onClick={() => navigate(generatePath(`${location.pathname}/create`), { state: { date } })}
           color="inherit"
           size="large"
           {...props}
