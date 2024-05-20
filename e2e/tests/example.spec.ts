a419db3f0d14f5ff40cdbb2edbc08070b53216a8
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-05-20 14:25:37 +0300

    Update example tests to ensure E2E environment gets data from mock API

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("");
+  await page.goto("/entries/week/20");
   await expect(page).toHaveTitle(/Keijo/);
+  await expect(page.locator("html")).toContainText("Hieno tuote");
 });
