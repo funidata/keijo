5f90a12ae09ec2d1d952a9d38f31d8a26921a155
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-05-23 12:57:11 +0300

    Add tooltip props override

--- web/src/components/LabelledIconButton.tsx
+++ web/src/components/LabelledIconButton.tsx
@@ -1,17 +1,22 @@
-import { IconButton, IconButtonProps, Tooltip } from "@mui/material";
+import { IconButton, IconButtonProps, Tooltip, TooltipProps } from "@mui/material";
 
 type LabelledIconButtonProps = IconButtonProps & {
   /**
    * Text to show in tooltip.
    */
   label: string;
+  tooltipProps?: Partial<TooltipProps>;
 };
 
 /**
  * Icon button labelled with tooltip for better UX.
  */
-const LabelledIconButton = ({ label, ...iconButtonProps }: LabelledIconButtonProps) => (
-  <Tooltip title={label} arrow placement="bottom">
+const LabelledIconButton = ({
+  label,
+  tooltipProps,
+  ...iconButtonProps
+}: LabelledIconButtonProps) => (
+  <Tooltip title={label} arrow placement="bottom" {...tooltipProps}>
     <IconButton size="large" color="inherit" aria-label={label} {...iconButtonProps} />
   </Tooltip>
 );
