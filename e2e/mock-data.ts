5d15acd37e73ea732581dc8d8146a6a0dc467935
Kimi Kuru <kimi.kuru@funidata.fi>
2024-05-24 16:11:12 +0300

    Add helper to get mock entries

--- e2e/mock-data.ts
+++ e2e/mock-data.ts
@@ -3,14 +3,28 @@ import path from "path";
 import { XMLParser } from "fast-xml-parser";
 
 const parser = new XMLParser();
-const doc = parser.parse(
+const dimensionDoc = parser.parse(
   readFileSync(path.resolve(__dirname, "../nv-mock/data/dimensionlist.nv.xml")),
+);
+
+const entryDoc = parser.parse(
+  readFileSync(path.resolve(__dirname, "../nv-mock/data/getworkdays.nv.xml")),
 );
 
 const dimensions: Array<{
   Name: string;
   DimensionDetails: { DimensionDetail: Array<{ Name: string }> };
-}> = doc.Root.DimensionNameList.DimensionName;
+}> = dimensionDoc.Root.DimensionNameList.DimensionName;
+
+const workdays: {
+  Workday: Array<{
+    Date: string;
+    WorkdayHour: {
+      Hours: string | number;
+      Dimension: Array<{ DimensionName: string; DimensionItem: string }>;
+    };
+  }>;
+} = entryDoc.Root.WorkDays;
 
 const getMockProductNames = () => {
   const names = dimensions
@@ -38,6 +52,33 @@ const getMockClientNames = () => {
     ?.find((x) => x.Name === "4 Asiakas")
     ?.DimensionDetails.DimensionDetail.map((x) => x.Name)!;
   return names;
+};
+
+export const getMockEntries = (): Array<{
+  date: string;
+  hours: string;
+  fields: Array<{
+    DimensionName: string;
+    DimensionItem: string;
+  }>;
+}> => {
+  return workdays.Workday.flatMap((wd) => {
+    if (Array.isArray(wd.WorkdayHour)) {
+      return wd.WorkdayHour.map((hour) => ({
+        date: wd.Date,
+        hours: String(hour.Hours).replace(",", "."),
+        fields: hour.Dimension,
+      }));
+    } else {
+      return [
+        {
+          date: wd.Date,
+          hours: String(wd.WorkdayHour.Hours).replace(",", "."),
+          fields: wd.WorkdayHour.Dimension,
+        },
+      ];
+    }
+  });
 };
 
 export { getMockProductNames, getMockActivityNames, getMockIssueNames, getMockClientNames };
