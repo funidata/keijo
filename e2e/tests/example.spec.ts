0ed5df1d9517eb1b4e8bca38db2b5133d85ddc18
Kimi Kuru <kimi.kuru@funidata.fi>
2024-05-24 16:12:14 +0300

    Refactor mock data test

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,13 +1,12 @@
 import { Page, expect } from "@playwright/test";
 import { test } from "../playwright.config";
 import { TFunction } from "i18next";
-import {
-  getMockProductNames,
-  getMockActivityNames,
-  getMockIssueNames,
-  getMockClientNames,
-} from "mock-data";
-import dayjs, { Dayjs } from "dayjs";
+import { getMockEntries } from "mock-data";
+import { Dayjs } from "dayjs";
+
+const mockEntries = getMockEntries();
+
+console.log(mockEntries);
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
@@ -26,35 +25,30 @@ test.describe("Landing page", () => {
     await checkAppBar(page, t);
   });
 
-  // TODO: refactor
-  test("should have mock entries", async ({ page, dayjs }) => {
-    await page.goto("/entries/week/20");
-    const date = dayjs().week(20).startOf("week");
-    // Monday mock entries
-    const mondayDetails = page.locator(".MuiAccordionDetails-root").first();
-    await expect(mondayDetails.getByText("5:00")).toBeVisible();
-    await expect(mondayDetails.getByText("Toteutus")).toBeVisible();
-    await expect(mondayDetails.getByText("Hieno Tuote")).toBeVisible();
-    // Tuesday mock entries
-    const tuesdayDetails = page
-      .locator("div", {
-        has: page.getByRole("button", { name: date.add(1, "day").format("dd l") }),
-      })
-      .last();
-    await expect(tuesdayDetails).toBeVisible();
-    await expect(tuesdayDetails.getByText("Hieno tuote")).toBeVisible();
-    await expect(tuesdayDetails.getByText("Hintava tuote")).toBeVisible();
-    await expect(tuesdayDetails.getByText("Toteutus")).toBeVisible();
-    await expect(tuesdayDetails.getByText("Suunnittelu")).toBeVisible();
-    await expect(tuesdayDetails.getByText("3:30")).toHaveCount(2);
-    const thursdayDetails = page
-      .locator("div", {
-        has: page.getByRole("button", { name: date.add(3, "day").format("dd l") }),
-      })
-      .last();
-    await expect(thursdayDetails.getByText("Hintava tuote")).toBeVisible();
-    await expect(thursdayDetails.getByText("Suunnittelu")).toBeVisible();
-    await expect(thursdayDetails.getByRole("heading", { name: "3:00" })).toBeVisible();
+  test("Should have mock entries", async ({ page, dayjs }) => {
+    for (const entry of mockEntries) {
+      const date = dayjs(entry.date);
+      await page.goto(`/entries/week/${date.week()}`);
+      const workdayEntryList = page
+        .locator("div", {
+          has: page.getByRole("button", { name: date.format("dd l") }),
+        })
+        .last()
+        .getByRole("list");
+
+      const minute = dayjs()
+        .minute(Number(entry.hours) * 60)
+        .minute();
+      const hour = dayjs().hour(Number(entry.hours)).hour();
+      console.log(minute, hour);
+      let entryRows = workdayEntryList.getByRole("listitem");
+      // Filter row by field texts
+      for (const field of entry.fields) {
+        entryRows = entryRows.filter({ hasText: new RegExp(`${field.DimensionItem}`) });
+      }
+      entryRows = entryRows.filter({ hasText: new RegExp(`${hour}:${minute}`) });
+      await expect(entryRows).toBeVisible();
+    }
   });
 });
 
