ef4d2fcc7b35347e86f64713b18aa77bccf36c76
Kimi Kuru <kimi.kuru@funidata.fi>
2024-06-05 15:07:37 +0300

    Use jira and session modules to app

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,14 @@ import { useContainer } from "class-validator";
 import { AppModule } from "./app.module";
 import { ConfigService } from "./config/config.service";
 import { AppLogger } from "./logger/app-logger";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
 
-const options = process.env.NODE_ENV === "development" ? { cors: { origin: "*" } } : {};
+const options =
+  process.env.NODE_ENV === "development"
+    ? { cors: { origin: "http://localhost:3000", credentials: true } }
+    : {};
 
 (async () => {
   const app = await NestFactory.create(AppModule, { bufferLogs: true, ...options });
@@ -13,5 +19,21 @@ const options = process.env.NODE_ENV === "development" ? { cors: { origin: "*" }
   app.useLogger(new AppLogger(configService));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    // TODO: change default store to pg store
+    session({
+      name: configService.config.session.name,
+      secret: configService.config.session.secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(cookieParser());
   await app.listen(configService.config.port);
 })();
