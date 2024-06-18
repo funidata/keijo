b14c16f408fa3e218eb5bf4ec69c7e84587396da
Kimi Kuru <kimi.kuru@funidata.fi>
2024-06-18 14:42:34 +0300

    Reuse dimensionComboBox to avoid duplicate code

--- web/src/components/entry-dialog/DimensionComboBox.tsx
+++ web/src/components/entry-dialog/DimensionComboBox.tsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@apollo/client";
-import { Autocomplete, TextField } from "@mui/material";
-import { Control, ControllerProps, FieldValues, UseFormReturn } from "react-hook-form";
+import { Autocomplete, AutocompleteProps, FormControl, Grid, TextField } from "@mui/material";
+import { Control, Controller, ControllerProps, FieldValues, UseFormReturn } from "react-hook-form";
 import { FindDimensionOptionsDocument } from "../../graphql/generated/graphql";
-import DimensionController from "./DimensionController";
 
 type DimensionComboBoxProps<T extends FieldValues> = {
   form: UseFormReturn<T>;
   name: "product" | "activity" | "issue" | "client";
   title: string;
   rules?: ControllerProps["rules"];
+  autoCompleteProps?: Partial<
+    AutocompleteProps<string, boolean | undefined, boolean | undefined, boolean | undefined>
+  >;
 };
 
 const DimensionComboBox = <T extends FieldValues>({
@@ -16,33 +18,39 @@ const DimensionComboBox = <T extends FieldValues>({
   name,
   title,
   rules,
+  autoCompleteProps,
 }: DimensionComboBoxProps<T>) => {
   const { data } = useQuery(FindDimensionOptionsDocument);
   const options = data?.findDimensionOptions[name] || [];
 
   return (
-    <DimensionController
-      control={form.control as unknown as Control<FieldValues>}
-      name={name}
-      rules={rules}
-      render={({ field: { value, onChange } }) => (
-        <Autocomplete
-          value={value}
-          onChange={(_, value) => onChange(value)}
-          options={options}
-          autoHighlight
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label={title}
-              onChange={onChange}
-              error={!!form.formState.errors[name]}
-              helperText={form.formState.errors[name]?.message as string}
+    <Grid item xs={12} md={6}>
+      <FormControl fullWidth>
+        <Controller
+          control={form.control as unknown as Control<FieldValues>}
+          name={name}
+          rules={rules}
+          render={({ field: { value, onChange } }) => (
+            <Autocomplete
+              value={value}
+              onChange={(_, value) => onChange(value)}
+              options={options}
+              autoHighlight
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label={title}
+                  onChange={onChange}
+                  error={!!form.formState.errors[name]}
+                  helperText={form.formState.errors[name]?.message as string}
+                />
+              )}
+              {...autoCompleteProps}
             />
           )}
         />
-      )}
-    />
+      </FormControl>
+    </Grid>
   );
 };
 
