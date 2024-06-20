53f12b97ac88c9e69f3cdf52cae502af223499ce
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-06-20 08:42:12 +0300

    Run database migrations in CI before E2E tests

--- server/migration-config.ts
+++ server/migration-config.ts
@@ -2,10 +2,10 @@ import { DataSource } from "typeorm";
 
 export default new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "keijo_dev",
+  host: process.env.DATABASE_HOST || "localhost",
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USERNAME || "postgres",
+  password: process.env.DATABASE_PASSWORD || "postgres",
+  database: process.env.DATABASE_NAME || "keijo_dev",
   migrations: ["src/database/migrations/*"],
 });
