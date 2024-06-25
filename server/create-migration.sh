9249e0020e24a79526532ca240a4711ef1192f4e
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-06-25 13:52:08 +0300

    Use inline function in migration:create command instead of script file

--- server/create-migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script exists because I could not figure out how to append a positional
-# argument to an NPM script and I also don't want to remember the migration
-# directory path every time I need to create one. Hohhoijjaa.
-npm run typeorm migration:create src/database/migrations/$1
