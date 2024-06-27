30006ffc753839dc3101ea7b1fadf07e70e4833b
Kimi Kuru <kimi.kuru@funidata.fi>
2024-06-27 16:24:12 +0300

    Fix token refresh on auth failure

--- web/src/jira/axiosInstance.ts
+++ web/src/jira/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useNotificationState } from "../components/global-notification/useNotification";
 import { jiraApiUrl, keijoJiraApiUrl } from "./jiraConfig";
-import { queryClient } from "./queryClient";
+
 import { JiraAuthLink } from "./components/JiraAuthLink";
 
 const axiosJira = axios.create({
@@ -12,19 +12,6 @@ const axiosKeijo = axios.create({
   baseURL: keijoJiraApiUrl,
   withCredentials: true,
 });
-
-axiosJira.interceptors.response.use(
-  (res) => res,
-  async (err) => {
-    if (err.response.status === 403 || err.response.status === 401) {
-      await queryClient.fetchQuery({
-        queryKey: ["accessToken"],
-        queryFn: async () => (await axiosKeijo.get("/refresh")).data,
-      });
-    }
-    return Promise.reject(err);
-  },
-);
 
 axiosKeijo.interceptors.response.use(
   (res) => res,
@@ -38,6 +25,29 @@ axiosKeijo.interceptors.response.use(
       });
     }
 
+    return Promise.reject(err);
+  },
+);
+
+/*
+  Try to 
+**/
+axiosJira.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    if (err.response.status === 403 || err.response.status === 401) {
+      const token = (await axiosKeijo.get("/refresh")).data;
+      if (token && err?.config && !err.config._retry) {
+        err.config._retry = true;
+        err.config.headers["Authorization"] = "Bearer " + token.access_token;
+        return axiosJira(err.config);
+      }
+    }
+    useNotificationState.getState().setNotification({
+      message: "Failed to fetch issue data from Jira: " + err.response.status,
+      type: "error",
+      autoHide: true,
+    });
     return Promise.reject(err);
   },
 );
