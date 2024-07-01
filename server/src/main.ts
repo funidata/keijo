a1ed3dfee2629a565a2ecb0811f580b6664efcb6
Kimi Kuru <kimi.kuru@funidata.fi>
2024-07-01 13:41:28 +0300

    Refactor session creation

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,7 @@ import { AppModule } from "./app.module";
 import { ConfigService } from "./config/config.service";
 import { AppLogger } from "./logger/app-logger";
 import cookieParser from "cookie-parser";
-import session from "express-session";
-import pgSession from "connect-pg-simple";
-import pg from "pg";
+import { createSession } from "./jira/session-setup";
 
 const options =
   process.env.NODE_ENV === "development"
@@ -25,31 +23,7 @@ const options =
   app.useLogger(new AppLogger(configService));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
-  const { username, password, host, port, name } = configService.config.database;
-  const pgPool = new pg.Pool({
-    database: name,
-    user: username,
-    password,
-    host,
-    port,
-  });
-  app.use(
-    session({
-      store: new (pgSession(session))({
-        pool: pgPool,
-        createTableIfMissing: true,
-      }),
-      name: configService.config.session.name,
-      secret: configService.config.session.secret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        maxAge: 90 * 24 * 60 * 60 * 1000,
-      },
-    }),
-  );
+  app.use(createSession(configService));
   app.use(cookieParser());
   await app.listen(configService.config.port);
 })();
