683216bb11f8b5711b1977be2051e5e886ec90a3
Kimi Kuru <kimi.kuru@funidata.fi>
2024-07-02 16:38:03 +0300

    Add trust proxy env var

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { useContainer } from "class-validator";
 import { AppModule } from "./app.module";
 import { ConfigService } from "./config/config.service";
@@ -18,11 +19,15 @@ const options =
     : {};
 
 (async () => {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true, ...options });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+    ...options,
+  });
   const configService = app.get(ConfigService);
   app.useLogger(new AppLogger(configService));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.set("trust proxy", configService.config.trustProxyIps);
   app.use(createSession(configService));
   app.use(cookieParser());
   await app.listen(configService.config.port);
