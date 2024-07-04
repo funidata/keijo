794181486f6da40fa6d281a2c7321628cf01bbec
Kimi Kuru <kimi.kuru@funidata.fi>
2024-07-04 10:42:28 +0300

    Refactor token guard as suggested

--- server/src/jira/token.guard.ts
+++ server/src/jira/token.guard.ts
@@ -1,14 +1,16 @@
-import { Injectable, ExecutionContext, CanActivate, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Request } from "express";
-import { SessionData } from "express-session";
 
 @Injectable()
 export class SessionTokenGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
-    const req: Request = context.switchToHttp().getRequest();
-    const session: SessionData = req.session;
-    if (!session.user || !session.user.refreshToken || !session.user.accessToken)
-      throw new UnauthorizedException();
-    return true;
+    const request = context.switchToHttp().getRequest<Request>();
+    const session = request.session;
+
+    if (session.user?.refreshToken && session.user.accessToken) {
+      return true;
+    }
+
+    return false;
   }
 }
