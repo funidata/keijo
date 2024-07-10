5d3f356d78e3b085f6f2c147c78b2f9a16333cc3
Kimi Kuru <kimi.kuru@funidata.fi>
2024-07-10 16:01:34 +0300

    Add translations for hide weekend

--- web/src/components/workday-browser/WorkdayList.tsx
+++ web/src/components/workday-browser/WorkdayList.tsx
@@ -1,16 +1,25 @@
 import { useQuery } from "@apollo/client";
-import { Paper } from "@mui/material";
+import { Box, Button, Collapse, Paper } from "@mui/material";
 import { range } from "lodash";
 import useDayjs from "../../common/useDayjs";
-import { FindWorkdaysDocument } from "../../graphql/generated/graphql";
+import { Entry, FindWorkdaysDocument } from "../../graphql/generated/graphql";
 import WorkdayAccordion from "../workday-accordion/WorkdayAccordion";
 import LoadingIndicator from "./LoadingIndicator";
 import TotalHours from "./TotalHours";
 import { useWorkdayBrowserParams } from "./useWorkdayBrowserParams";
+import { useState } from "react";
+import { isWeekend } from "../../common/workdayUtils";
+import WeekendIcon from "@mui/icons-material/Weekend";
+import WeekendOutlinedIcon from "@mui/icons-material/WeekendOutlined";
+import { IconButton } from "@mui/material";
+import { Dayjs } from "dayjs";
+import { t } from "i18next";
 
 const WorkdayList = () => {
   const dayjs = useDayjs();
+
   const { from, to, formattedFrom, formattedTo } = useWorkdayBrowserParams();
+  const [checked, setChecked] = useState(false);
 
   const { data } = useQuery(FindWorkdaysDocument, {
     variables: { start: formattedFrom, end: formattedTo },
@@ -25,6 +34,10 @@ const WorkdayList = () => {
   const normalizedStart = from.hour(0).minute(0).second(0).millisecond(0);
   const normalizedEnd = to.hour(0).minute(0).second(0).millisecond(0);
 
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   // Construct requested date range to include days without entries.
   const workdays = range(normalizedEnd.diff(normalizedStart, "day") + 1).map((i) => {
     const date = normalizedStart.add(i, "day");
@@ -34,14 +47,41 @@ const WorkdayList = () => {
     };
   });
 
+  const dividedWorkdays = workdays.reduce<{ date: Dayjs; entries: Entry[] }[][]>((arr, curr) => {
+    if (
+      arr.length === 0 ||
+      isWeekend(curr.date) !== isWeekend(arr.slice(-1)[0].slice(-1)[0].date)
+    ) {
+      arr.push([curr]);
+    } else {
+      arr.slice(-1)[0].push(curr);
+    }
+    return arr;
+  }, []);
+
   return (
     <>
       <TotalHours workdays={workdays} />
+
       <Paper>
-        {workdays.map((wd) => (
-          <WorkdayAccordion workday={wd} key={wd.date.toString()} />
-        ))}
+        {dividedWorkdays.map((wdArr) => {
+          if (isWeekend(wdArr[0].date)) {
+            return (
+              <Collapse in={checked}>
+                {wdArr.map((wd) => (
+                  <WorkdayAccordion workday={wd} key={wd.date.toString()} />
+                ))}
+              </Collapse>
+            );
+          }
+          return wdArr.map((wd) => <WorkdayAccordion workday={wd} key={wd.date.toString()} />);
+        })}
       </Paper>
+      <Box textAlign="center" mt={2}>
+        <Button onClick={handleChange} size="small" color="secondary" sx={{ borderRadius: 28 }}>
+          {!checked ? t("controls.showWeekend") : t("controls.hideWeekend")}
+        </Button>
+      </Box>
     </>
   );
 };
