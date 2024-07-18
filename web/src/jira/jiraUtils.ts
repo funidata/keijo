73c093e52ea459a564c79c9d8abb243b1f56347d
Kimi Kuru <kimi.kuru@funidata.fi>
2024-07-18 13:22:10 +0300

    Clean up key matching logic

--- web/src/jira/jiraUtils.ts
+++ web/src/jira/jiraUtils.ts
@@ -22,36 +22,33 @@ export const chunkArray = <T>(array: T[], size: number) => {
  * Find a word in a string that matches to some Jira issue key(s)
  * by containing atleast the issueKey part preceding the dash (project key).
  */
-export const findWordInKeys = (searchFilter: string, issueKeys: string[]) =>
+export const findWordInKeys = (searchFilter: string, issueKeys: string[]) => {
+  const words = searchFilter.toLowerCase().match(/\S+/gi);
+  if (!words || words.length < 2) {
+    return;
+  }
+  return words.find(
+    (word) =>
+      word &&
+      issueKeys.some(
+        (key) =>
+          word.trim() &&
+          word.split("-")[0] === key.toLowerCase().split("-")[0] &&
+          key.toLowerCase().includes(word),
+      ),
+  );
+};
+
+export const findKeysIncludingWord = (word: string, issueKeys: string[]) =>
+  issueKeys.filter((key) => key.toLowerCase().includes(word.toLowerCase()));
+
+export const stringWithoutWord = (searchFilter: string, word: string) =>
   searchFilter
     .toLowerCase()
     .trim()
     .split(" ")
-    .find(
-      (word) =>
-        word &&
-        issueKeys.some(
-          (key) =>
-            word.trim() &&
-            key.toLowerCase().trim().split("-")[0] === word.trim().split("-")[0] &&
-            key.toLowerCase().trim().includes(word.trim()),
-        ),
-    );
-
-export const findKeysIncludingWord = (word: string, issueKeys: string[]) =>
-  word
-    ? issueKeys.filter((key) => key.toLowerCase().trim().includes(word?.toLowerCase().trim()))
-    : [];
-
-export const removeWord = (searchFilter: string, word: string) =>
-  word
-    ? searchFilter
-        .toLowerCase()
-        .trim()
-        .split(" ")
-        .filter((text) => text.trim() !== word)
-        .join(" ")
-    : searchFilter;
+    .filter((text) => text.trim() !== word)
+    .join(" ");
 
 export const connectToJira = () => {
   window.location.href = keijoJiraApiUrl;
