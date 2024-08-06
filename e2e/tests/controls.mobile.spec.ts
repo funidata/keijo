a2a2b9b28c9671f97216de905f2d42f3f6056275
Kimi Kuru <kimi.kuru@funidata.fi>
2024-08-06 12:01:24 +0300

    Add e2e tests for jira connect and disconnect

--- e2e/tests/controls.mobile.spec.ts
+++ e2e/tests/controls.mobile.spec.ts
@@ -40,3 +40,33 @@ test.describe("Browse week mobile", () => {
     await expect(page).toHaveURL(`/entries/week/${startingWeek + jump}`);
   });
 });
+
+test.describe("Connect Jira Mobile", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/entries/week/`);
+  });
+
+  test("Connect to Jira", async ({ page, t }) => {
+    await page.getByRole("banner").getByLabel(t("controls.openMenu")).click();
+    await page.getByRole("button", { name: t("controls.jiraConnect") }).click();
+    await expect(page).toHaveURL(/.*id\.atlassian\.com\/login.*/);
+  });
+
+  test("Disconnect from Jira", async ({ page, t, context }) => {
+    await page.route("**/access-token", (route) =>
+      route.fulfill({
+        status: 200,
+        body: "{'access_token': 'test_token','cloud_id': 'test_cloud_id' }",
+      }),
+    );
+    let sessionRemoveRequested = false;
+    context.on("request", (request) => {
+      if (request.url().includes("/remove-session")) {
+        sessionRemoveRequested = true;
+      }
+    });
+    await page.getByRole("banner").getByLabel(t("controls.openMenu")).click();
+    await page.getByRole("button", { name: t("controls.jiraDisconnect") }).click();
+    expect(sessionRemoveRequested).toBe(true);
+  });
+});
