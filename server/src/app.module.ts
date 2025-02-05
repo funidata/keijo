e30def5e5fcf8b20795d3e1059d8c98b3408b1f0
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-02-05 12:58:37 +0200

    Fix wildcard in static serve module's exclude path to conform with Express v5

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,17 +8,17 @@ import { ConfigModule } from "./config/config.module";
 import { DatabaseModule } from "./database/database.module";
 import graphQlModuleConfig from "./graphql-module-config";
 import { appGuards } from "./guards/app-guards";
+import { JiraModule } from "./jira/jira.module";
 import { LoggerModule } from "./logger/logger.module";
 import { NetvisorModule } from "./netvisor/netvisor.module";
 import { SessionModule } from "./session/session.module";
 import { UserSettingsModule } from "./user-settings/user-settings.module";
-import { JiraModule } from "./jira/jira.module";
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public"),
-      exclude: ["/graphql/(.*)"],
+      exclude: ["/graphql/{*_}"],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleConfig),
     CacheModule,
