2d6e9d01258c88436a0ffd409b28c3346118d834
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-05-06 14:56:52 +0300

    Relocate i18n fixture

--- e2e/fixtures/fixtures.ts
+++ e2e/fixtures/fixtures.ts
@@ -1,6 +1,6 @@
 import { mergeTests } from "@playwright/test";
-import { i18nFixture } from "../i18n-fixture";
 import { appBarFixture } from "./app-bar/app-bar.fixture";
 import { dayjsFixture } from "./dayjs.fixture";
+import { i18nFixture } from "./i18n.fixture";
 
 export const test = mergeTests(dayjsFixture, i18nFixture, appBarFixture);
