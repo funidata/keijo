e5b108bfba29717dad5b664a9bf0446626100b93
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-05-09 13:05:23 +0300

    Reset database before each test

--- e2e/fixtures/auth.fixture.ts
+++ e2e/fixtures/auth.fixture.ts
@@ -1,13 +1,18 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 export const authFixture = test.extend({
   page: async ({ page }, use, testInfo) => {
-    // TODO: Reset database for given `workerIndex` first.
+    const workerIndex = testInfo.workerIndex.toString();
+
+    // Reset database for this worker.
+    // FIXME: API URL.
+    const response = await page.goto(`http://localhost:4001/dev/reset?id=${workerIndex}`);
+    expect(response?.ok()).toBe(true);
 
     // Use worker index as employee ID to separate test runners.
     await page.route("**/graphql", (route, request) => {
       const headers = request.headers();
-      headers["x-shib-employeeid"] = testInfo.workerIndex.toString();
+      headers["x-shib-employeeid"] = workerIndex;
       route.continue({ headers });
     });
 
