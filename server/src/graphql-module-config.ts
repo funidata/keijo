c6c665979181888dbfae340090a0a500b2b37d6b
Emily Hernandez <emily.hernandez@funidata.fi>
2026-06-11 09:02:33 +0300

    hotfix: Remove playground from use

--- server/src/graphql-module-config.ts
+++ server/src/graphql-module-config.ts
@@ -1,18 +1,9 @@
-import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-
-const plugins: NonNullable<ApolloDriverConfig["plugins"]> = [];
-
-if (process.env.NODE_ENV !== "production") {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins.push(ApolloServerPluginLandingPageLocalDefault({ includeCookies: true }) as any);
-}
 
 const graphQlModuleConfig: ApolloDriverConfig = {
   autoSchemaFile: true,
   driver: ApolloDriver,
   playground: false,
-  plugins,
 };
 
 export default graphQlModuleConfig;
