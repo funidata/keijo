a9dbcb0fe5a08d76ec37d7ced929609185b8d7c8
Emily Hernandez <emily.hernandez@funidata.fi>
2026-08-12 17:53:18 +0300

    KEIJO-46: Refactor templates layout

--- web/src/components/template-accordion/TemplateAccordion.tsx
+++ web/src/components/template-accordion/TemplateAccordion.tsx
@@ -1,42 +1,40 @@
-import { Box, Stack, Grid, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { visuallyHidden } from "@mui/utils";
 import { GetMySettingsDocument } from "../../graphql/generated/graphql";
 import CreateTemplateButton from "./AddTemplateButton";
 import EntryTemplateRow from "./EntryTemplateRow";
 import { useQuery } from "@apollo/client/react";
 import { useTranslation } from "react-i18next";
+import { useMemo } from "react";
 
 const TemplateAccordion = () => {
   const { data: settingsData } = useQuery(GetMySettingsDocument);
   const { t } = useTranslation();
 
+  const hasTemplates = useMemo(() => {
+    const templates = settingsData?.getMySettings.entryTemplates;
+    return templates?.length && templates.length > 0;
+  }, [settingsData]);
+
   return (
     <Box sx={{ mb: 4 }}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          mb: 2,
-        }}
-      >
-        <Typography component="h2" variant="h6">{t("titles.templates")}</Typography>
-        <CreateTemplateButton size="medium" />
-      </Box>
+      <Typography component="h2" variant="h6" sx={visuallyHidden}>
+        {t("titles.templates")}
+      </Typography>
+
       <Box>
         <Grid container spacing={1}>
-          {settingsData?.getMySettings.entryTemplates?.length ? (
+          {hasTemplates &&
             settingsData?.getMySettings.entryTemplates?.map((template) => (
               <Grid key={template.key} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-                <EntryTemplateRow key={template.key} entry={template} />
+                <EntryTemplateRow entry={template} />
               </Grid>
-            ))
-          ) : (
-            <Stack sx={{ justifyContent: "center", alignItems: "center", width: "100%" }}>
-              <Typography color="textDisabled" variant="body2">
-                {t("titles.noTemplates")}
-              </Typography>
-            </Stack>
-          )}
+            ))}
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+            <CreateTemplateButton />
+          </Grid>
         </Grid>
       </Box>
     </Box>
