784d775df69e9d3cf90cb189f7f38fca24f705e7
Emily Hernandez <emily.hernandez@funidata.fi>
2026-08-21 15:41:38 +0300

    KEIJO-200: Create hardcoded chart examples

--- web/src/components/workday-browser/WorkdayBrowser.tsx
+++ web/src/components/workday-browser/WorkdayBrowser.tsx
@@ -4,9 +4,11 @@ import WorkdayList from "./WorkdayList";
 import Box from "@mui/material/Box";
 import { EntryContextProvider } from "./entry-context/EntryContextProvider";
 import TemplateAccordion from "../template-accordion/TemplateAccordion";
+import OverviewWrapper from "../overview/OverviewWrapper";
 
 const WorkdayBrowser = () => (
   <>
+    <OverviewWrapper />
     <EntryContextProvider>
       <ListControls />
       <Box sx={{ mt: { sx: 0, sm: 3 }, mb: { xs: 4, sm: 1 } }}>
